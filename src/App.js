b6a338df2f3be154f655241a39522ceacdaf2fd8
Jeffery Cosby <jcoz1968@gmail.com>
2019-06-14 20:26:12 -0500

    fill in page components, routes

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <>
+    <h1>About Me</h1>
+    <p>
+      Welcome to my blog! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Aliquam euismod, tortor in facilisis dignissim, elit odio hendrerit ex, a sagittis dui diam id neque.
+      Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      In faucibus posuere dui at aliquet. In hac habitasse platea dictumst. Nulla quis enim odio.
+      Cras malesuada sapien consequat gravida tempor. Integer eleifend, diam non vehicula ultrices,
+      diam purus fermentum odio, sit amet porttitor tortor sapien eget erat.
+    </p>
+    <p>
+      Etiam accumsan erat id nisl lobortis ultricies.
+      Sed aliquam fringilla semper. Pellentesque vehicula massa nunc, eget mattis sem maximus in.
+      Duis convallis molestie elit a efficitur. Etiam ultricies leo ligula, non sollicitudin nibh luctus vel.
+      Praesent pretium purus eget massa ultricies, fringilla ullamcorper eros sagittis. Vestibulum lorem libero,
+      egestas id dui at, sodales varius urna. Fusce euismod diam nibh, et congue nibh gravida non.
+      In hac habitasse platea dictumst. Suspendisse ultrices mauris eu risus commodo maximus. Aenean sed pretium felis.
+      Fusce in efficitur sapien. Integer justo leo, porttitor a magna at, tincidunt
+      sollicitudin leo. Nulla ornare mi nec blandit scelerisque. Aliquam justo quam, vulputate sed erat quis,
+      tempor malesuada velit.
+    </p>
+  </>
+);
+
+export default AboutPage;
