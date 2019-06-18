4549c42211e9b9a7fbc797a7b895db010b4f5f79
Jeffery Cosby <jcoz1968@gmail.com>
2019-06-17 21:24:21 -0500

    react hooks

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from '../pages/NotFoundPage';
 
 const ArticlePage = ({ match }) => {
-
   const name = match.params.name;
   const article = articleContent.find(article => article.name === name);
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  useEffect(() => { 
+    setArticleInfo({ upvotes: 3 });
+  });
+
   if (!article) return <NotFoundPage />;
   const otherArticles = articleContent.filter(article => article.name !== name);
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times.</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
