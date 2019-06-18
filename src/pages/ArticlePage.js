412b1122a256b4fe35c19dcc7a54a3041b7f4889
Jeffery Cosby <jcoz1968@gmail.com>
2019-06-17 22:17:06 -0500

    comments list component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from '../pages/NotFoundPage';
+import CommentsList from '../components/CommentsList';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -29,6 +30,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
